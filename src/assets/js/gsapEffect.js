9f33e12604cefcd9b624d5c9161c169080f4527d
rlanrid <rladnrider77@gmail.com>
2023-12-15 17:56:56 +0900

    1215작업

---
+++ src/assets/js/gsapEffect.js
@@ -0,0 +1,55 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+// ScrollTrigger 플러그인을 등록하고 함수 정의
+gsap.registerPlugin(ScrollTrigger);
+
+export function weird() {
+    // 02. 여러개 이질감 표현하기
+    gsap.utils.toArray(".text__gif").forEach(item => {
+        gsap.to(item, {
+            ease: "none",
+            duration: 0.5,
+            scrollTrigger: {
+                trigger: item,
+                start: "center center",
+                end: "bottom top",
+                scrub: 0.5
+            }
+        })
+    });
+}
+
+export function split() {
+
+    // 02 모든 텍스트 분리하기
+    document.querySelectorAll(".split").forEach(text => {
+        let splitWrap = text.innerText.split("").join("</span><span aria-hidden='true'>");
+        text.innerHTML = "<span aria-hidden='true'>" + splitWrap + "</span>";
+        text.setAttribute("aria-label", text.innerText);
+    })
+
+    gsap.utils.toArray(".split").forEach(text => {
+        gsap.from(text.querySelectorAll("span"), {
+            yPercent: -100,
+            autoAlpha: 0,
+            duration: 2,
+            ease: "circ.out",
+            stagger: {
+                amount: 0.5,
+                from: "start"
+            },
+            scrollTrigger: {
+                trigger: text,
+                start: "top bottom",
+                end: "+400",
+                markers: true
+            }
+        });
+    })
+
+    const ani1 = gsap.timeline();
+    ani1.from("#picture .text__gif.g1", { xPercent: 20, autoAlpha: 0, duration: 0.5, delay: 1 })
+        .from("#picture .text__gif.g2", { xPercent: -20, autoAlpha: 0, duration: 0.5 })
+        .from("#picture .text__gif.g3", { yPercent: 20, autoAlpha: 0, duration: 0.5 })
+
+}
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,7 @@
 import { mouse } from "./mouse.js"
 import { menu } from "./menu.js"
-import { picture } from "./picture.js"
+import { weird } from "./gsapEffect.js"
+import { split } from "./gsapEffect.js"
 import { smooth } from "./smooth.js"
 import { menubar } from "./menubar.js"
 import { title } from "./appear.js"
@@ -8,7 +9,8 @@ import { title } from "./appear.js"
 window.addEventListener("load", function () {
     mouse();
     menu();
-    // picture();
+    weird();
+    split();
     smooth();
     menubar();
     title();
